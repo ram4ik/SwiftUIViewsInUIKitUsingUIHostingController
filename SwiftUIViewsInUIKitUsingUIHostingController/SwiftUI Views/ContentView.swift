3671bdf52ceb9c4726f97101541bbbbc6e83dbde
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-04-20 20:07:51 +0300

    init

---
+++ SwiftUIViewsInUIKitUsingUIHostingController/SwiftUI Views/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftUIViewsInUIKitUsingUIHostingController
+//
+//  Created by Ramill Ibragimov on 20.04.2020.
+//  Copyright © 2020 Ramill Ibragimov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .background(Color.green)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
