397687cf7c495f74be081127e502af689eb0fd4a
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-04-20 19:56:05 +0300

    Initial Commit

---
+++ SwiftUIViewsInUIKitUsingUIHostingController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftUIViewsInUIKitUsingUIHostingController
+//
+//  Created by Ramill Ibragimov on 20.04.2020.
+//  Copyright © 2020 Ramill Ibragimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
